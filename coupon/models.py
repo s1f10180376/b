3a966936548d11a004dead5605bd058451ce2016
Miku Endo <s1F101803589@iniad.org>
2020-11-25 18:30:01 +0900

    モデルの変更
    Couponクラスに取得日のdateフィールドと期間のtermフィールドを追加
    デフォルト値を変更

--- coupon/models.py
+++ coupon/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class Shop(models.Model):
+    shop_name = models.CharField(max_length=128)
+    coupon_name = models.CharField(max_length=128)
+    coupon_content = models.TextField(default="")
+    term = models.IntegerField(null=True)
+
+class Coupon(models.Model):
+    coupon_name = models.CharField(max_length=128)
+    coupon_content = models.TextField(default="")
+    date = models.DateTimeField(default=timezone.now)
+    term = models.IntegerField(null=True)
+    limit = models.DateField(null=True)
+    shop = models.ForeignKey(Shop, on_delete=models.CASCADE)
