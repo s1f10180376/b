153f3ebe484374d5884eba3f1a757bf857a33f9f
Miku Endo <s1F101803589@iniad.org>
2020-12-02 16:42:55 +0900

    Add signup function

--- config/urls.py
+++ config/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path(r'', include('coupon.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/', include('accounts.urls')), 
 ]
