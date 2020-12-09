72cb2109e29a60c6a322d78e8e3dee6de4216fa3
Miku Endo <s1F101803589@iniad.org>
2020-12-09 22:55:27 +0900

    Edit coupon\urls.py
    update urls

--- coupon/urls.py
+++ coupon/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('get/<int:shop_id>', views.get, name='get'),
-    path('detail/<int:coupon_id>', views.detail, name='detail'),
-    path('use/<int:coupon_id>', views.use, name='use'),
+    path('coupon/get/<int:shop_id>', views.get, name='get'),
+    path('coupon/detail/<int:coupon_id>', views.detail, name='detail'),
+    path('coupon/use/<int:coupon_id>', views.use, name='use'),
 ]
