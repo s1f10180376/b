7daa20a7076e1e0da1c205352c72aba5132d019a
Miku Endo <s1F101803589@iniad.org>
2020-12-31 00:07:47 +0900

    ホーム画面へのリンクの修正、クーポン使用時にダイアログ表示
    ロゴをクリックしたときのホーム画面へのリンクを修正しました。
    クーポン使用時に「使用する」を押すと、ダイアログで確認をするようにしました。

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,6 +5,5 @@ from . import views
 #app_name = 'accounts'
 
 urlpatterns = [
-    path('', views.home, name='home'),
     path('signup/', views.SignUpView.as_view(), name='signup'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -8,6 +8,3 @@ class SignUpView(generic.CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'accounts/signup.html'
-
-def home(request):
-    return render(request, 'coupon/home.html')
