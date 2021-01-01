c3a3a784bb3a64086ec7b7a65654f30c5465d3f3
Miku Endo <s1F101803589@iniad.org>
2021-01-02 02:45:50 +0900

    ユーザー名変更機能を追加
    ユーザ名を変更できる機能を追加しました。

--- accounts/urls.py
+++ accounts/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('signup/', views.SignUpView.as_view(), name='signup'),
     path('password_change/', views.PasswordChange.as_view(), name='password_change'),
     path('password_change/done/', views.PasswordChangeDone.as_view(), name='password_change_done'),
+    path('username_change/', views.UserUpdateView.as_view(), name='username_change'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.views import PasswordChangeView, PasswordChangeDoneView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
+from django.utils.decorators import method_decorator
 from django.views import generic
+from django.views.generic import UpdateView
 
 
 # Create your views here.
@@ -25,3 +29,13 @@ class PasswordChange(LoginRequiredMixin, PasswordChangeView):
 class PasswordChangeDone(LoginRequiredMixin,PasswordChangeDoneView):
     """パスワード変更完了"""
     template_name = 'accounts/password_change_done.html'
+
+@method_decorator(login_required, name='dispatch')
+class UserUpdateView(UpdateView):
+    model = User
+    fields = ('username',)
+    template_name = 'accounts/username_change.html'
+    success_url = reverse_lazy('account')
+
+    def get_object(self):
+        return self.request.user
